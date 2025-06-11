9634d33b7396cd44b95bd6d8de651304b4eb43f5
Emiri Sato <emirisato.1003@gmail.com>
2025-06-10 20:00:00 -0700

    initial commit

--- src/pages/exercise/Exercise.jsx
+++ src/pages/exercise/Exercise.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
 
 export default function Exercise() {
-    
+    const baseUrl = `https://exercisedb.p.rapidapi.com/exercises/name/${name}`;
     const name = '';
-    const exerciseUrl = `https://exercisedb.p.rapidapi.com/exercises/name/${name}`;
     const exerciseOptions = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': '8da38a4a0bmsh8786479b1290ccep10d763jsn0dd3c70a2bf8',
+            'x-rapidapi-key': import.meta.env.EXERCISE_API_KEY,
             'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
         }
     };
