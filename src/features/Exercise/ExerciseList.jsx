a956bb5ba4cc91c3b91089b68a11d3457e5dc610
Emiri Sato <emirisato.1003@gmail.com>
2025-06-17 16:53:20 -0700

    adding pagination

--- src/features/Exercise/ExerciseList.jsx
+++ src/features/Exercise/ExerciseList.jsx
@@ -2,20 +2,44 @@ import React, { useEffect, useState } from 'react';
 import { Box, Stack, Button, TextField } from '@mui/material';
 import SearchExercises from '../../shared/ExerciseViewForm/SearchExercises';
 import FilterExercise from '../../shared/ExerciseViewForm/FilterExercise';
+// import Pagination from '@mui/material/Pagination';
+// import PaginationItem from '@mui/material/PaginationItem';
+// import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 // import '../../service/service';
 import ExerciseListCard from './ExerciseListCard';
 import styles from './ExerciseList.module.css';
 import { fetchData, exerciseOptions } from '../../utils/fetchData';
 import { MdError } from "react-icons/md";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { useSearchParams } from 'react-router';
 
 const baseUrl = `https://exercisedb.p.rapidapi.com/exercises`;
 const mockBaseUrl = `/api/exercises`;
 
 export default function ExerciseList({ exercisesList, setExercisesList, bodyPart, setBodyPart, filteredExercises, errorMessage }) {
-    console.log(exercisesList.map(item => {
-        // const allItems = item.flat()
-        return [...new Set(item.secondaryMuscles)];
-    }));
+    const [searchParams, setSearchParams] = useSearchParams();
+    const itemsPerPage = 15;
+    const currentPage = parseInt(searchParams.get('page') || '1');
+    const indexOfFirstExercise = itemsPerPage * (currentPage - 1);
+    const indexOfLastExercise = currentPage * itemsPerPage;
+    const totalPages = Math.ceil(filteredExercises.length / itemsPerPage);
+    const currentExercises = filteredExercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setSearchParams({ page: currentPage - 1 });
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setSearchParams({ page: currentPage + 1 });
+        }
+    };
+
     return (
         <main>
             {errorMessage ?
@@ -40,11 +64,16 @@ export default function ExerciseList({ exercisesList, setExercisesList, bodyPart
                             <section className={styles.exercises}>
                                 <h1 style={{ margin: '0 0 .7em 3em' }}>{bodyPart} Exercises</h1>
                                 <div className={styles.exerciseLists}>
-                                    {filteredExercises.map(exercise =>
+                                    {currentExercises.map(exercise =>
                                         <ExerciseListCard
                                             key={exercise.id}
                                             exercise={exercise}
                                         />)}
+                                </div>
+                                <div className="paginationControls">
+                                    <button onClick={() => handlePreviousPage()} disabled={currentPage === 1}><IoIosArrowDropleftCircle /></button>
+                                    <span>{currentPage} of {totalPages}</span>
+                                    <button onClick={() => handleNextPage()} disabled={currentPage === totalPages}><IoIosArrowDroprightCircle /></button>
                                 </div>
                             </section>
                         </>
