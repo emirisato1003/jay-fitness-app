fa07cd36221340c5ba8325d139498bc3f6207ecc
Emiri Sato <emirisato.1003@gmail.com>
2025-06-24 12:00:03 -0700

    create a Not Found component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router';
 import Layout from './Layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Exercises from './pages/Exercises';
 import ExerciseListDetail from './features/ExerciseDetail/ExerciseListDetail';
 import DetailRelatedList from './features/RelatedExercise/DetailRelatedList';
@@ -18,10 +19,13 @@ function App() {
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='exercise' element={<Exercises />} />
+
           <Route path='exercise/:id' element={<ExerciseListDetail />}>
             <Route index element={<DetailRelatedVideo />} />
             <Route path='relatedLists' element={<DetailRelatedList />} />
           </Route>
+          
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default function NotFound() {
     return (
         <div>
-            <h1>404 : Page Not Found.</h1>
+            <h1>404</h1>
+            <h2>Sorry, the page you were looking for was not found.</h2>
+            <button>
+                <Link to='/'>Return to home</Link>
+            </button>
         </div>
     );
 }
